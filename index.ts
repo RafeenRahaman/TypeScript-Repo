2bfd574e67bbb9d76a56ec0d3eaddf9bd086bd20
RafeenRahaman <68629271+RafeenRahaman@users.noreply.github.com>
2023-08-18 18:32:55 -0400

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,15 @@
+// let aString: string = 'hello world'
+// console.log(aString)
+// aString = "I am rafeen"
+// function calcCircumference<T>(radius: number, texts?: T): number {
+//     let circumference = 2*Math.PI*radius
+
+//     if (texts) {
+//         console.log(texts)
+//     }
+//     return circumference
+// }
+// let circleRadius: number = 5
+// let circleCircumference = calcCircumference(circleRadius)
+// console.log(`Circle's circumference with radius ${circleRadius} is equal to ${circleCircumference}`)
+
